2d680d0f7a1e61fbd7b3359d77732947c9a1e93f
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-09-04 18:20:47 +0000

    v2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express    = require("express"); // call express
+var express = require("express"); // call express
 var bodyParser = require("body-parser");
 var nodemailer = require("nodemailer");
-var app        = express();
+var app = express();
 
 
 
@@ -12,58 +12,63 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("index");
 });
 
-
-
-app.post("/send", (req, res) => {
-    var name = (req.body.name);
-    var output = `
-    <h3> Nieuw bericht van ${req.body.name}.<h3>
-    <h5> Details <h5>
-    <ul>
-        <li>Naam : ${req.body.name}</li>
-        <li>Email : ${req.body.email}</li>
-        <li>Telefoon : ${req.body.tel}</li>
-    </ul>
-    <p>Bericht : ${req.body.bericht}<p>
-    `
-    
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-        user: 'mailserver163@gmail.com',
-        pass: 'ma1ls3rv3r'
-    }
+app.get("/gallerij", function(req, res) {
+    res.render("gallerij");
 });
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"www.passiermuziekemmen.nl" <mailserver163@gmail.com>', // sender address
-        to: 'niek_losenoord@hotmail.com', // list of receivers
-        subject: name + ' Heeft een bericht gestuurd via de website.', // Subject line
-        text: '', // plain text body
-        html: output // html body
-    };
 
-    var msg = "Bedankt voor je bericht! Binnen 24 uur zijn we bij je terug."
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        
-        res.render("contact-bedankt")
-        
-    });
-});
+
+
+// app.post("/send", (req, res) => {
+//     var name = (req.body.name);
+//     var output = `
+//     <h3> Nieuw bericht van ${req.body.name}.<h3>
+//     <h5> Details <h5>
+//     <ul>
+//         <li>Naam : ${req.body.name}</li>
+//         <li>Email : ${req.body.email}</li>
+//         <li>Telefoon : ${req.body.tel}</li>
+//     </ul>
+//     <p>Bericht : ${req.body.bericht}<p>
+//     `
+
+//     // create reusable transporter object using the default SMTP transport
+//     const transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         auth: {
+//             user: 'mailserver163@gmail.com',
+//             pass: 'ma1ls3rv3r'
+//         }
+//     });
+//     // setup email data with unicode symbols
+//     let mailOptions = {
+//         from: '"www.passiermuziekemmen.nl" <mailserver163@gmail.com>', // sender address
+//         to: 'niek_losenoord@hotmail.com', // list of receivers
+//         subject: name + ' Heeft een bericht gestuurd via de website.', // Subject line
+//         text: '', // plain text body
+//         html: output // html body
+//     };
+
+//     var msg = "Bedankt voor je bericht! Binnen 24 uur zijn we bij je terug."
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return console.log(error);
+//         }
+//         console.log('Message sent: %s', info.messageId);
+//         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+//         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+//         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+//         res.render("contact-bedankt")
+
+//     });
+// });
 
 // app = express.createServer();
 
@@ -98,6 +103,6 @@ app.post("/send", (req, res) => {
 //   smtpTransport.close(); 
 //     }); });
 
-app.listen(process.env.PORT, process.env.IP, function(){ // tell node to listen & define a port to view app
-    console.log("Passier server starting...");
+app.listen(process.env.PORT, process.env.IP, function() { // tell node to listen & define a port to view app
+    console.log("Server starting...");
 });
